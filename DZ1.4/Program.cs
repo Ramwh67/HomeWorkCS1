727942e89043ff8d664be63a7d04b551c9b53ae8
Ramwh <vasilkovd_67@mail.ru>
2022-10-17 21:19:41 +0400

    Добавлено решение четвёртой задачи

---
+++ DZ1.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Введите целое число ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            if ( num >= i)
+            {
+                Console.Write("Все чётные числа от 1 до " + num + ": ");
+                while ( i <= num )
+                {
+                    Console.Write( i + " ");
+                    i = i + 2;
+                }
+            }
+            else Console.WriteLine("Введите число побольше");
+        }
+
+    }
+}
