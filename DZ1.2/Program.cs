8bfa535767afc4de0fa349dcdbc746599325a3cb
Ramwh <vasilkovd_67@mail.ru>
2022-10-17 20:50:24 +0400

    Добавлено решение второй задачи

---
+++ DZ1.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Введите первое число ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третье число ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 >= num2 && num1 >= num3)
+            {
+                Console.WriteLine("Наибольшее число " + num1);
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                Console.WriteLine("Наибольшее число " + num2);
+            }
+            else 
+            {
+                Console.WriteLine("Наибольшее число " + num3);
+            }
+        }
+    }
+}
