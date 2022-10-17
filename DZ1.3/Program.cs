aecc7304fe56c2136576646b870d539644386983
Ramwh <vasilkovd_67@mail.ru>
2022-10-17 21:04:54 +0400

    Добавлено решение третьей задачи

---
+++ DZ1.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Введите число ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            if ( num % 2 == 0 )
+            {
+                Console.WriteLine("Число является чётным");
+            }
+            else
+            {
+                Console.WriteLine("Число является нечётным");
+            }
+        }
+    }
+}
