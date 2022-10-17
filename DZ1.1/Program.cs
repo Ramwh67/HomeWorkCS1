f82d82304cfd7ed0ea7ca4875dcfcf5c425df189
Ramwh <vasilkovd_67@mail.ru>
2022-10-17 20:29:00 +0400

    Добавлено решение первой задачи

---
+++ DZ1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Введите первое число ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int b = Convert.ToInt32(Console.ReadLine());
+           
+           if (a > b) 
+           {
+                Console.WriteLine("Наибольшее число " + a);
+            
+           }
+            else
+            {
+                Console.WriteLine("Наибольшее число " + b);
+            }
+        }
+    }
+}
